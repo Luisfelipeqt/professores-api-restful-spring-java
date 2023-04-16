421030f1f227efa7c700b560cb3487d914fc86c4
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-04-16 01:47:06 -0300

    Configurando ambiente e PROFILES

---
+++ ProjectProfessors/src/main/java/br/com/projectprofessors/ProjectProfessors/ProjectProfessorsApplication.java
@@ -0,0 +1,13 @@
+package br.com.projectprofessors.ProjectProfessors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProfessorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectProfessorsApplication.class, args);
+	}
+
+}
